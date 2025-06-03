b0bea552e3e2ea1e86ec04ece4d69ad5cdeca5da
maiaio <maiabodoni@gmail.com>
2025-06-04 00:11:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,151 @@
+let player, buzz, buzz2;
+
+let minSize = 100;
+let maxSize = 600;
+let maxShapeSize = 500;
+let minShapeSize = 20;
+let showStatic = false;
+let startScreen = true;
+
+let sliderX, sliderWidth, sliderHeight;
+let dragging = false;
+
+let shapeX1, shapeY1, shapeSize1;
+let shapeX2, shapeY2, shapeSize2;
+
+let playingSound = 0; // 0 = none, 1 = buzz, 2 = buzz2
+
+function preload() {
+  player = loadSound('sounds/sound.mp3');
+  buzz = loadSound('sounds/buzz.mp3');
+  buzz2 = loadSound('sounds/buzz2.mp3');
+}
+
+function setup() {
+  createCanvas(600, 600);
+  background(0);
+  noLoop();
+
+  player.setLoop(true);
+  player.play();
+
+  sliderX = width / 2;
+  sliderWidth = width - 100;
+  sliderHeight = 20;
+}
+
+function draw() {
+  background(0);
+  let s = map(width, minSize, maxSize, minShapeSize, maxShapeSize);
+  let shapeType = int(random(5));
+
+  shapeX1 = random(width / 2);
+  shapeY1 = random(height);
+  shapeSize1 = s;
+
+  shapeX2 = width - shapeX1;
+  shapeY2 = height - shapeY1;
+  shapeSize2 = s;
+
+  fill(0);
+  drawShape(shapeType, shapeX1, shapeY1, shapeSize1);
+  fill(255);
+  drawShape(shapeType, shapeX2, shapeY2, shapeSize2);
+
+  if (showStatic) drawStatic();
+
+  if (checkCollision()) {
+    if (playingSound !== 2) {
+      stopAllSounds();
+      buzz2.play();
+      playingSound = 2;
+    }
+  }
+
+  drawSlider();
+  adjustSound();
+}
+
+function drawShape(type, x, y, s) {
+  if (type === 0) {
+    ellipse(x, y, s, s);
+  } else if (type === 1) {
+    ellipse(x, y, s, s);
+    rect(x - s / 2, y - s / 2, s, s);
+  } else if (type === 2) {
+    triangle(x, y - s / 2, x - s / 2, y + s / 2, x + s / 2, y + s / 2);
+  } else if (type === 3) {
+    ellipse(x, y, s * 1.5, s);
+  } else if (type === 4) {
+    rect(x, y, s * 1.5, s / 2);
+  }
+}
+
+function drawSlider() {
+  fill(150);
+  rect(50, height - 40, sliderWidth, sliderHeight, 10);
+
+  fill(255, 0, 0);
+  ellipse(sliderX, height - 30, 20, 20);
+}
+
+function mousePressed() {
+  if (startScreen) {
+    startScreen = false;
+  }
+
+  if (dist(mouseX, mouseY, sliderX, height - 30) < 10) {
+    dragging = true;
+  }
+
+  redraw();
+}
+
+function mouseDragged() {
+  if (!startScreen && dragging) {
+    sliderX = constrain(mouseX, 50, 50 + sliderWidth);
+    let newWidth = int(map(sliderX, 50, 50 + sliderWidth, minSize, maxSize));
+    resizeCanvas(newWidth, newWidth);
+    showStatic = true;
+    redraw();
+  }
+}
+
+function mouseReleased() {
+  dragging = false;
+}
+
+function mouseMoved() {
+  let over1 = dist(mouseX, mouseY, shapeX1, shapeY1) < shapeSize1 / 2;
+  let over2 = dist(mouseX, mouseY, shapeX2, shapeY2) < shapeSize2 / 2;
+
+  if ((over1 || over2) && playingSound !== 1) {
+    stopAllSounds();
+    buzz.play();
+    playingSound = 1;
+  }
+}
+
+function stopAllSounds() {
+  buzz.stop();
+  buzz2.stop();
+  playingSound = 0;
+}
+
+function adjustSound() {
+  let volume = map(width, minSize, maxSize, 0.0, 1.0);
+  player.setVolume(volume);
+}
+
+function checkCollision() {
+  let d = dist(shapeX1, shapeY1, shapeX2, shapeY2);
+  let minD = (shapeSize1 + shapeSize2) / 2;
+  return d < minD;
+}
+
+function drawStatic() {
+  for (let i = 0; i < 1000; i++) {
+    stroke(random(255));
+    point(random(width), random(height));
+  }
+}
